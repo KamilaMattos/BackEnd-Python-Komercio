f82a262758cddf6c1c54a7a0b71823878b786309
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-10-21 16:56:22 -0300

    FIX: renamed local models urls files

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path("products/", views.ListCreateProductView.as_view()),
-    path("products/<pk>/", views.RetrieveUpdateProductView.as_view()),
+    path("products/", views.ListCreateProductView.as_view(), name="product-view"),
+    path(
+        "products/<pk>/",
+        views.RetrieveUpdateProductView.as_view(),
+        name="product-detail",
+    ),
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,17 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from . import views
 
 urlpatterns = [
-    path("accounts/", views.ListCreateAccountView.as_view()),
+    path("accounts/", views.ListCreateAccountView.as_view(), name="account-register"),
     path("login/", ObtainAuthToken.as_view()),
-    path("accounts/newest/<int:num>/", views.ListAccountByDateView.as_view()),
-    path("accounts/<pk>/", views.UpdateAccountView.as_view()),
-    path("accounts/<pk>/management/", views.DeactivateAccountView.as_view()),
+    path(
+        "accounts/newest/<int:num>/",
+        views.ListAccountByDateView.as_view(),
+        name="list-view",
+    ),
+    path("accounts/<pk>/", views.UpdateAccountView.as_view(), name="account-update"),
+    path(
+        "accounts/<pk>/management/",
+        views.DeactivateAccountView.as_view(),
+        name="account-manager",
+    ),
 ]
